663ea5f44452d2e9931dba1667dee5bc5e365843
Michael Small <chickenhat22@gmail.com>
2025-03-10 21:48:17 -0500

    feat: `linkedQueryParams` example

--- src/app/user.component.ts
+++ src/app/user.component.ts
@@ -5,15 +5,21 @@ import { JsonPipe } from '@angular/common';
 import { injectQueryParams } from 'ngxtension/inject-query-params';
 import { injectRouteData } from 'ngxtension/inject-route-data';
 import { injectRouteFragment } from 'ngxtension/inject-route-fragment';
+import {FormsModule} from '@angular/forms'
+import { linkedQueryParam } from 'ngxtension/linked-query-param';
 
 @Component({
   selector: 'app-user',
   template: `
     <pre>params: {{params() | json}}</pre>
     <pre>paramsId: {{paramsId() | json}}</pre>
+
     <pre>queryParams: {{queryParams() | json}}</pre>
+    <p>linkedQueryParam is two way binding for query params: it syncs to when QPs are sent (hardcoded to buttons), or it will update the QPs if you set them (like this input)</p>
+    <label for="linkedQP">Linked Query Param</label>
+    <input [(ngModel)]="count" name="linkedQP" type="number" />
+
     <pre>routeData: {{routeData() | json}}</pre>
-    <pre>routeFragment: {{routeFragment() | json}}</pre>
 
     @if (!userStore.loading()) {
         <pre>user: {{user() | json}}</pre>
@@ -22,7 +28,7 @@ import { injectRouteFragment } from 'ngxtension/inject-route-fragment';
     }
   `,
   styles: ``,
-  imports: [JsonPipe],
+  imports: [JsonPipe, FormsModule],
   providers: [UserStore]
 })
 export class UserComponent {
@@ -34,7 +40,11 @@ export class UserComponent {
     paramsId = injectParams('id'); // soon there will be a default value + parsing to number and whatnot
     queryParams = injectQueryParams(); // can also be passed a param
     routeData = injectRouteData();
-    routeFragment = injectRouteFragment();
+
+    count = linkedQueryParam('value', {
+        parse: (value) => parseInt(value ?? '0', 10),
+        stringify: (value) => value.toString(),
+    });
 
     ngOnInit() {
         this.userStore.getOne(this.paramsId())
